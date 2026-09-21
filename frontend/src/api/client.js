0d1050e3f9d7fce6dc13bd83b9ec1283f86f0f19
Bishal Bastakoti <bastakotibishal6@gmail.com>
2026-09-21 22:13:14 +0545

    fixedurl

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const API_BASE_URL =
-  'https://bishalvoid-001-site1.ktempurl.com/api';
+  'https://bishalcare-001-site1.ctempurl.com/api';
 
 const client = axios.create({
   baseURL: API_BASE_URL,
